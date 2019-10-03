dede59107fbb226767550e2a71788cf606c3335f
James Lee <jameswlee109@gmail.com>
2019-10-02 22:19:14 -0500

    shopping list test complete

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,28 @@
+INSERT INTO blogful_articles (title, content, date_published)
+VALUES
+  ('blog 1', 'some blog content for this article', now() - '21 days'::INTERVAL),
+  ('blog 2', 'some blog content for this articles', now() - '21 days'::INTERVAL),
+  ('blog 3', 'some blog content for this articlez', now() - '21 days'::INTERVAL),
+  ('blog 4', 'some blog content for this articled', now() - '21 days'::INTERVAL),
+  ('blog 5', 'some blog content for this articlef', now() - '21 days'::INTERVAL),
+  ('blog 6', 'some blog content for this articleg', now() - '9 days'::INTERVAL),
+  ('blog 7', 'some blog content for this articlew', now() - '9 days'::INTERVAL),
+  ('blog 8', 'some blog content for this articleo', now() - '9 days'::INTERVAL),
+  ('blog 9', 'some blog content for this articley', now() - '9 days'::INTERVAL),
+  ('blog 10', 'some blog content for this articleh', now() - '9 days'::INTERVAL),
+  ('blog 11', 'some blog content for this articleu', now() - '9 days'::INTERVAL),
+  ('blog 12', 'some blog content for this articlem', now() - '7 days'::INTERVAL),
+  ('blog 13', 'some blog content for this articleq', now() - '7 days'::INTERVAL),
+  ('blog 14', 'some blog content for this articlee', now() - '7 days'::INTERVAL),
+  ('blog 15', 'some blog content for this articlet', now() - '5 days'::INTERVAL),
+  ('blog 16', 'some blog content for this articleb', now() - '5 days'::INTERVAL),
+  ('blog 17', 'some blog content for this articlec', now() - '3 days'::INTERVAL),
+  ('blog 18', 'some blog content for this articlex', now() - '3 days'::INTERVAL),
+  ('blog 19', 'some blog content for this articlel', now() - '3 days'::INTERVAL),
+  ('blog 20', 'some blog content for this articlep', now() - '2 days'::INTERVAL),
+  ('blog 21', 'some blog content for this articlei', now() - '1 days'::INTERVAL),
+  ('blog 22', 'some blog content for this articler', now()),
+  ('blog 23', 'some blog content for this articlej', now()),
+  ('blog 24', 'some blog content for this articlek', now())
+;
+  
